bb92d53af5b86152ca63582bc4ecfe4844525549
nnsf0sker <47502186+nnsf0sker@users.noreply.github.com>
2019-03-15 23:40:49 +0300

    Add files via upload
    "Задача.pdf" - просто исправил название pdf-файла с заданием;
    "reference_result.txt" - лог-файл с результатами всех тестов (не дан по условию - это результат работы программы);
    "norm_results.txt" - эталонный лог-файл, данный по условию задачи (переименован, чтобы не перезаписывался);
    "sc.py" - программа

--- sc.py
+++ sc.py
@@ -1,158 +1,208 @@
 import os
 
-directrory = os.getcwd()
-
-f = open('reference_result_1.txt', 'tw')
-
-a = "12.345"
-print(float(a[0:1])+1)
-
-def MFSP (input_string):
+def MFSP(input_string):
     if input_string.startswith("Memory Working Set Current = "):
-        Mb1_index = input_string.index(' Mb')
-        Mb2_index = input_string.index(' Mb', Mb1_index+1)
-        first_num = float(input_string[29:Mb1_index])
-        second_num = float(input_string[Mb1_index+31:Mb2_index])
-        return first_num, second_num
+        return float(input_string[(input_string.index(' Mb') + 31):-3])
     else:
-        return -1, -1
+        return -1
 
-def MESH (input_string):
+
+def MESH(input_string):
     if input_string.startswith("MESH::Bricks: Total="):
-        Mb1_index = input_string.index('Gas')
-        return (int(input_string[20:Mb1_index-1]))
+        return (int(input_string[20:(input_string.index('Gas') - 1)]))
     else:
         return -1;
 
-for i in os.listdir(path=directrory + "/" + "logs"):
-    for j in os.listdir(path=directrory + "/" + "logs" + "/" + i):
-        hasOutput = 0
-        if (os.path.exists(directrory + "/" + "logs" + "/" + i + "/" + j + "/" + "ft_reference") != True):
-            hasOutput = 1
-            f.write("FAIL: "+i+"/"+j+"\n")
+manyOutputError = 0
+
+directrory = os.getcwd()  # Абсолютный путь к папке, где находится программа
+
+logFolderPath = os.path.join(directrory, "logs")  # Абсолютный путь к папке log
+
+f = open('reference_result.txt', 'tw')  # Файл, куда будем писать результаты тестов
+
+for i in os.listdir(path=logFolderPath):  # Основной цикл, пробегающий по всем подпапкам всех папок log
+    firstSubfoldPath = os.path.join(logFolderPath, i)
+    for j in os.listdir(path=firstSubfoldPath):
+        secondSubfoldPath = os.path.join(firstSubfoldPath, j)  # Абсолютный путь к текущей папке
+        relCurrentFolderPath = os.path.join(i, j)  # Относительный путь папки текущего теста (для вывода)
+        hasOutput = 0  # Флаг, был ли вывод для данного теста
+        currentOutputFile = open(os.path.join(secondSubfoldPath, "report.txt"), 'tw')
+
+        if os.path.exists(os.path.join(secondSubfoldPath, "ft_reference")) != True:
+            # hasOutput = 1
+            f.write("FAIL: " + relCurrentFolderPath + "/\n")
             f.write("directory missing: ft_reference\n")
+            currentOutputFile.write("FAIL: " + relCurrentFolderPath + "/\n")
+            currentOutputFile.write("directory missing: ft_reference\n")
+            currentOutputFile.close()
             # Записываем в файл отсутствие папки ft_reference
             continue
-        if (os.path.exists(directrory + "/" + "logs" + "/" + i + "/" + j + "/" + "ft_run") != True):
-            hasOutput = 1
-            f.write("FAIL: "+i+"/"+j+"\n")
-            f.write("directory missing: ft_reference\n")
-            # Записываем в файл отсутствие папки ft_run
+
+        if os.path.exists(os.path.join(secondSubfoldPath, "ft_run")) != True:
+            # hasOutput = 1
+            f.write("FAIL: " + relCurrentFolderPath + "/\n")
+            f.write("directory missing: ft_run\n")
+            currentOutputFile.write("FAIL: " + relCurrentFolderPath + "/\n")
+            currentOutputFile.write("directory missing: ft_run\n")
+            currentOutputFile()
+            # Записываем в файл отсутсвие папки ft_run
             continue
 
-        rel_path = directrory + "/" + "logs" + "/" + i + "/" + j + "/"
-        run_path = rel_path + "ft_run"
-        ref_path = rel_path + "ft_reference"
+        ft_runFolderPath = os.path.join(secondSubfoldPath, "ft_run")  # Абсолютный путь до папки ft_run текущего теста
+        ft_referenceFolderPath = os.path.join(secondSubfoldPath, "ft_reference")  # Абсолютный путь до папки ft_reference текущего теста
 
-        ListOfDirsRun = os.listdir(path=run_path)
-        ListOfDirsReference = os.listdir(path=ref_path)
+        ft_runDirs = set()  # Множество, содержащее подпапки ft_run
+        ft_referenceDirs = set()  # Множество, содержащее подпапки ft_reference
 
-        ft_runBuff = []
-        ft_referenceBuff = []
-        set_ft_runBuff = set()
-        set_ft_referenceBuff = set()
-
-        for k in os.walk(rel_path + "ft_run"):
+        for k in os.walk(ft_runFolderPath):
             for l in range(len(k[2])):
                 if (k[2][l].endswith(".stdout") == True):
-                    set_ft_runBuff.add(os.path.relpath(k[0], run_path)+"/"+k[2][l])
+                    ft_runDirs.add(os.path.relpath(os.path.join(k[0], k[2][l]), ft_runFolderPath))
 
-        for k in os.walk(rel_path + "ft_reference"):
+        for k in os.walk(ft_referenceFolderPath):
             for l in range(len(k[2])):
                 if (k[2][l].endswith(".stdout") == True):
-                    set_ft_referenceBuff.add(os.path.relpath(k[0], ref_path)+"/"+k[2][l])
+                    ft_referenceDirs.add(os.path.relpath(os.path.join(k[0], k[2][l]), ft_referenceFolderPath))
 
-        if (set_ft_runBuff != set_ft_referenceBuff):
+        if ft_runDirs != ft_referenceDirs:  # Вывод в случае, если *.stdout файлы в ft_run и ft_reference не совпадают
             hasOutput = 1
-            f.write("FAIL: "+i+"/"+j+"\n")
+            f.write("FAIL: " + relCurrentFolderPath + "/\n")
+            currentOutputFile.write("FAIL: " + relCurrentFolderPath + "/\n")
 
-            setr = list(set_ft_referenceBuff - set_ft_runBuff)
-            if(len(setr)!=0):
+            buffSet = list(ft_referenceDirs - ft_runDirs)
+            if (len(buffSet) != 0):
                 f.write("In ft_run there are missing files present in ft_reference: ")
-                for m in range(len(setr)):
-                    f.write("'"+setr[m]+"'")
-                    if m == (len(setr)-1):
+                currentOutputFile.write("In ft_run there are missing files present in ft_reference: ")
+
+                for m in range(len(buffSet)):
+                    f.write("'" + buffSet[m] + "'")
+                    currentOutputFile.write("'" + buffSet[m] + "'")
+                    if m == (len(buffSet) - 1):
                         f.write("\n")
+                        currentOutputFile.write("\n")
                     else:
                         f.write(" ")
+                        currentOutputFile.write(" ")
 
-            setr = list(set_ft_runBuff - set_ft_referenceBuff)
-            if (len(setr) != 0):
+            buffSet = list(ft_runDirs - ft_referenceDirs)
+            if (len(buffSet) != 0):
                 f.write("In ft_run there are extra files files not present in ft_reference: ")
-                for m in range(len(setr)):
-                    f.write("'" + setr[m] + "'")
-                    if m == (len(setr)-1):
+                currentOutputFile.write("In ft_run there are extra files files not present in ft_reference: ")
+                for m in range(len(buffSet)):
+                    f.write("'" + buffSet[m] + "'")
+                    currentOutputFile.write("'" + buffSet[m] + "'")
+                    if m == (len(buffSet) - 1):
                         f.write("\n")
+                        currentOutputFile.write("\n")
                     else:
                         f.write(" ")
+                        currentOutputFile.write(" ")
 
+            currentOutputFile.close()
             continue
 
-        setr = list(set_ft_runBuff)
-        for k in range(len(setr)):
-            run_file = open(run_path+"/"+setr[k], 'tr')
+        buffSet = list(ft_runDirs)
+        for k in range(len(buffSet)):
+            ft_runFile = open(os.path.join(ft_runFolderPath, buffSet[k]), 'tr')
             nLine = 1
             isError = 0
             isFlag = 0
 
-            for line in run_file:
+            for line in ft_runFile:
                 if "ERROR" in line.upper():
-                    hasOutput = 1
-                    f.write("FAIL: " + i + "/" + j + "\n")
-                    f.write(setr[k]+"("+str(nLine)+") "+ line)
-                    isError = 1
-                    break
+                    if hasOutput == 0:
+                        hasOutput = 1
+                        f.write("FAIL: " + relCurrentFolderPath + "/\n")
+                        f.write(buffSet[k] + "(" + str(nLine) + ") " + line)
+                        currentOutputFile.write("FAIL: " + relCurrentFolderPath + "/\n")
+                        currentOutputFile.write(buffSet[k] + "(" + str(nLine) + ") " + line)
+                        currentOutputFile.close()
+                        isError = 1
+                        break
+                    else:
+                        manyOutputError = 1
+                        #print("ERROR!!!")
 
                 if line.startswith("Solver finished at"):
                     isFlag = 1
+                nLine = nLine + 1
 
-            if (isError==0)&(isFlag==0):
-                hasOutput = 1
-                f.write("FAIL: " + i + "/" + j + "\n")
-                f.write(setr[k]+": missing 'Solver finished at'\n")
+            if (isError == 0) & (isFlag == 0):
+                if hasOutput == 0:
+                    hasOutput = 1
+                    f.write("FAIL: " + relCurrentFolderPath + "/\n")
+                    f.write(buffSet[k] + ": missing 'Solver finished at'\n")
+                    currentOutputFile.write("FAIL: " + relCurrentFolderPath + "/\n")
+                    currentOutputFile.write(buffSet[k] + ": missing 'Solver finished at'\n")
+                    currentOutputFile.close()
+                else:
+                    manyOutputError = 1
+                    #print("ERROR!!!")
 
-            run_file.close()
+            ft_runFile.close()
 
-            if (isError==0)&(isFlag==1):
-                run_file = open(run_path + "/" + setr[k], 'tr')
-                ref_file = open(ref_path+"/"+setr[k], 'tr')
+            if (isError == 0) & (isFlag == 1):
+                ft_runFile = open(os.path.join(ft_runFolderPath, buffSet[k]), 'tr')
+                ft_referenceFile = open(os.path.join(ft_referenceFolderPath, buffSet[k]), 'tr')
 
                 runMaxim = -1
                 refMaxim = -1
                 runMESH = -1
                 refMESH = -1
-                for line in run_file:
-                    if MFSP(line)[1] > runMaxim:
-                        runMaxim = MFSP(line)[1]
-                    if MESH(line)>0:
+                for line in ft_runFile:
+                    if MFSP(line) > runMaxim:
+                        runMaxim = MFSP(line)
+                    if MESH(line) > 0:
                         runMESH = MESH(line)
 
-                for line in ref_file:
-                    if MFSP(line)[1] > refMaxim:
-                        refMaxim = MFSP(line)[1]
-                    if MESH(line)>0:
+                for line in ft_referenceFile:
+                    if MFSP(line) > refMaxim:
+                        refMaxim = MFSP(line)
+                    if MESH(line) > 0:
                         refMESH = MESH(line)
 
-                diffMax = max(runMaxim, refMaxim)/min(runMaxim, refMaxim)
-                diffMESH = max(runMESH, refMESH)/min(runMESH, refMESH)
-                # print(refMaxim, runMaxim, diff)
+                diffMax = max(runMaxim, refMaxim) / min(runMaxim, refMaxim)
+                diffMESH = max(runMESH, refMESH) / min(runMESH, refMESH)
 
-                if (diffMax > 4):
-                    hasOutput = 1
-                    f.write("FAIL: " + i + "/" + j + "\n")
-                    f.write(setr[k]+": different 'Memory Working Set Peak' (ft_run="+str(runMaxim)+", ft_reference="+str(refMaxim)+", rel.diff="+str(round(diffMax-1,2))+", criterion=4)\n")
+                if diffMax > 4:
+                    if hasOutput == 0:
+                        hasOutput = 1
+                        f.write("FAIL: " + relCurrentFolderPath + "/\n")
+                        f.write(buffSet[k] + ": different 'Memory Working Set Peak' (ft_run=" + str(
+                            runMaxim) + ", ft_reference=" + str(refMaxim) + ", rel.diff=" + str(
+                            round(diffMax - 1, 2)) + ", criterion=4)\n")
+                        currentOutputFile.write("FAIL: " + relCurrentFolderPath + "/\n")
+                        currentOutputFile.write(buffSet[k] + ": different 'Memory Working Set Peak' (ft_run=" + str(
+                            runMaxim) + ", ft_reference=" + str(refMaxim) + ", rel.diff=" + str(
+                            round(diffMax - 1, 2)) + ", criterion=4)\n")
+                        currentOutputFile.close()
+                    else:
+                        manyOutputError = 1
+                        #print("ERROR!!!")
 
-                if ((diffMESH-1) > 0.1):
-                    hasOutput = 1
-                    f.write("FAIL: " + i + "/" + j + "\n")
-                    f.write(setr[k] + ": different 'Total' of bricks (ft_run=" + str(runMESH) + ", ft_reference=" + str(refMESH) + ", rel.diff=" + str(round(diffMESH - 1, 2)) + ", criterion=0.1)\n")
-                ref_file.close()
+                if (diffMESH - 1) > 0.1:
+                    if hasOutput == 0:
+                        hasOutput = 1
+                        f.write("FAIL: " + relCurrentFolderPath + "/\n")
+                        f.write(buffSet[k] + ": different 'Total' of bricks (ft_run=" + str(runMESH) + ", ft_reference=" + str(
+                            refMESH) + ", rel.diff=" + str(round(diffMESH - 1, 2)) + ", criterion=0.1)\n")
+                        currentOutputFile.write("FAIL: " + relCurrentFolderPath + "/\n")
+                        currentOutputFile.write(
+                            buffSet[k] + ": different 'Total' of bricks (ft_run=" + str(runMESH) + ", ft_reference=" + str(
+                                refMESH) + ", rel.diff=" + str(round(diffMESH - 1, 2)) + ", criterion=0.1)\n")
+                        currentOutputFile.close()
+                    else:
+                        manyOutputError = 1
+                        #print("ERROR!!!")
+
+                ft_referenceFile.close()
+                ft_runFile.close()
 
         if hasOutput == 0:
             hasOutput = 1
-            f.write("OK: " + i + "/" + j + "\n")
-            run_file.close()
-
-
+            f.write("OK: " + relCurrentFolderPath + "/\n")
+            currentOutputFile.write("OK: " + relCurrentFolderPath + "/\n")
+            currentOutputFile.close()
 
 f.close()
